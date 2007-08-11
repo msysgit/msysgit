d9978a684366939a7e0682f7054ceda46e8b83ad
Steffen Prohaska <prohaska@zib.de>
2007-08-11 19:32:40 +0200

    GitMe: check if cygwin is in PATH; if so abort installer.
    The installer may have problems if cygwin is in the system PATH.
    If cygpath is known to the installation shell, the installer
    will abort before starting the installation.
    
    Signed-off-by: Steffen Prohaska <prohaska@zib.de>
    Signed-off-by: Dmitry Kakurin <Dmitry.Kakurin@gmail.com>

--- share/GitMe/setup-msysgit.sh
+++ share/GitMe/setup-msysgit.sh
@@ -12,7 +12,27 @@ error () {
     exit 1
 }
 
+echo -------------------------------------------------------
+echo Checking environment
+echo -------------------------------------------------------
+type cygpath >/dev/null 2>/dev/null && {
+    echo "Cygwin seems to be in your system path. This was detected"
+    echo "by trying to run cygpath, which was found by this shell."
+    echo ""
+    echo "Cygwin may cause severe problems, like crashes, if used in"
+    echo "combination with msysgit. Please remove Cygwin from you system"
+    echo "PATH environment variable."
+    echo ""
+    echo "For assistance on how to control your environment variables"
+    echo "you should consult Microsoft's knowlege base:"
+    echo "   Windows XP: http://support.microsoft.com/kb/310519"
+    echo "   Windows NT: http://support.microsoft.com/kb/100843"
+    echo ""
+    error "Can not install msysgit when Cygwin is in PATH."
+}
+echo "Environment is clean. Can install msysgit."
 
+echo
 echo -------------------------------------------------------
 echo Fetching the latest MSys environment
 echo -------------------------------------------------------
