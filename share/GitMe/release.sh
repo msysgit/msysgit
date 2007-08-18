90803f562da736bbe85fdce942bb0bf22031387e
Johannes Schindelin <johannes.schindelin@gmx.de>
2007-08-18 02:59:35 +0200

    GitMe: inline 7z's install script
    There is no need to maintain this script in its own file; furthermore,
    it does not need CR/LF (I actually tested it).  Therefore, we
    can just do away with the crlf attributes for 7z-install.txt.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- share/GitMe/release.sh
+++ share/GitMe/release.sh
@@ -30,6 +30,22 @@ cp "$SHARE"/setup-msysgit.sh ./ &&
 echo "Creating archive" &&
 cd .. &&
 7z a $OPTS7 "$TMPPACK" installer-tmp &&
-cat /share/7-Zip/7zSD.sfx "$SHARE"/7z-install.txt "$TMPPACK" > "$TARGET" &&
+(cat /share/7-Zip/7zSD.sfx &&
+ echo ';!@Install@!UTF-8!' &&
+ echo 'Title="GitMe: MinGW Git + MSys installation"' &&
+ echo 'BeginPrompt="This archive contains the minimal system needed to\nbootstrap the latest MinGW Git and MSys environment"' &&
+ echo 'CancelPrompt="Do you want to cancel MSysGit installation?"' &&
+ echo 'ExtractDialogText="Please, wait..."' &&
+ echo 'ExtractPathText="Where do you want to install MSysGit?"' &&
+ echo 'ExtractTitle="Extracting..."' &&
+ echo 'GUIFlags="8+32+64+256+4096"' &&
+ echo 'GUIMode="1"' &&
+ echo 'InstallPath="C:\\msysgit"' &&
+ echo 'OverwriteMode="2"' &&
+ echo 'RunProgram="%%T\installer-tmp\bin\sh.exe /setup-msysgit.sh"' &&
+ echo 'Delete="%%T\installer-tmp"' &&
+ echo 'RunProgram="%%T\bin\sh.exe --login -i"' &&
+ echo ';!@InstallEnd@!' &&
+ cat "$TMPPACK") > "$TARGET" &&
 echo Success! You\'ll find the new installer at $TARGET
 rm $TMPPACK
