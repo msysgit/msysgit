21eb05bd84c6954ae748f28492a6664ea2cb2a52
Johannes Schindelin <johannes.schindelin@gmx.de>
2007-08-18 02:33:56 +0200

    msysGit: we have 7zip installed in /share/7-Zip/ now
    We switched to the 7zSD self-extracting module a while ago; use that
    for msysGit, too.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- share/msysGit/release.sh
+++ share/msysGit/release.sh
@@ -27,6 +27,23 @@ LIST=list.txt
 sed "s|^|msysGit/|" > $LIST &&
 
 # make installer
-OPTS7="-m0=lzma -mx=9 -md=64M -sfx7z.sfx $TARGET" &&
+OPTS7="-m0=lzma -mx=9 -md=64M" &&
+/share/7-Zip/7z.exe a $OPTS7 "$TARGET".7z @$LIST &&
 
-"$PROGRAMFILES"/7-Zip/7z.exe a $OPTS7 @$LIST
+(cat /share/7-Zip/7zSD.sfx &&
+ echo ';!@Install@!UTF-8!' &&
+ echo 'Progress="yes"' &&
+ echo 'Title="GitMe: MinGW Git + MSys installation"' &&
+ echo 'BeginPrompt="This archive contains the complete system needed to\nbootstrap the latest MinGW Git and MSys environment"' &&
+ echo 'CancelPrompt="Do you want to cancel MSysGit installation?"' &&
+ echo 'ExtractDialogText="Please, wait..."' &&
+ echo 'ExtractPathText="Where do you want to install MSysGit?"' &&
+ echo 'ExtractTitle="Extracting..."' &&
+ echo 'GUIFlags="8+32+64+256+4096"' &&
+ echo 'GUIMode="1"' &&
+ echo 'InstallPath="C:\\msysgit"' &&
+ echo 'OverwriteMode="2"' &&
+ echo 'RunProgram="%%T\\msysGit\\bin\\sh.exe --login -i"' &&
+ echo ';!@InstallEnd@!7z' &&
+ cat "$TARGET".7z) > "$TARGET"
+
