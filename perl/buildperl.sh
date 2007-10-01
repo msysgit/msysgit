2c211911b1b259631734ffa3e271f3a0c79672e7
Simon Sasburg <Simon.Sasburg@gmail.com>
2007-10-01 23:46:36 +0200

    add script to do automated perl build
    run the script from within the perl-5.6.1/ directory created by installperl.sh, like this: "../buildperl.sh"

---
+++ perl/buildperl.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+PATH=/bin:$PATH
+./Configure -dE -D cc=gcc -U libc= -U libs= -U libsfiles= -U libsfound= -U libswanted= -D runnm=false -D usenm=false -D so=dll -D usedl=false -D firstmakefile=GNUmakefile -D exe_ext=.exe -D _exe=.exe -D archname=msys
+./Configure -S
+make
+echo > ext.libs
+make
