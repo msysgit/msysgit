8fbdabdf8381a02ca919d9b0d31ac75f8f4d6564
Johannes Schindelin <johannes.schindelin@gmx.de>
2007-10-05 01:55:35 +0100

    msysperl: cd to the correct directory in builperl.sh
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- perl/buildperl.sh
+++ perl/buildperl.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd /perl/perl-5.6.1
 PATH=/bin:$PATH
 ./Configure -dE -D cc=gcc -U libc= -U libs= -U libsfiles= -U libsfound= -U libswanted= -D runnm=false -D usenm=false -D so=dll -D usedl=false -D firstmakefile=GNUmakefile -D exe_ext=.exe -D _exe=.exe -D archname=msys
 ./Configure -S
