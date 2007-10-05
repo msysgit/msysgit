0e688cd8861100c8c08f8cea7d9f803953fefdd2
Simon Sasburg <Simon.Sasburg@gmail.com>
2007-10-05 22:59:08 +0200

    Completely prevent linking of -lm, instead of working around this during compilation.
    Previously we let the first 'make' fail, then clear ext.libs which was uses to link -lm, and then retry 'make'.
    Now we prevent linking -lm by removing it from @libs in ext/POSIX/Makefile.PL.
    
    Signed-off-by: Simon Sasburg <Simon.Sasburg@gmail.com>

--- perl/buildperl.sh
+++ perl/buildperl.sh
@@ -4,5 +4,3 @@ PATH=/bin:$PATH
 ./Configure -dE -D cc=gcc -U libc= -U libs= -U libsfiles= -U libsfound= -U libswanted= -D runnm=false -D usenm=false -D so=dll -D usedl=false -D firstmakefile=GNUmakefile -D exe_ext=.exe -D _exe=.exe -D archname=msys
 ./Configure -S
 LIB= make
-echo > ext.libs
-LIB= make
