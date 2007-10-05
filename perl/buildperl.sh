2cc8949d680b698a728f51c1b78cd940409c80db
Johannes Schindelin <johannes.schindelin@gmx.de>
2007-10-05 01:54:19 +0100

    msysperl: Prevent LIB from Visual Studio to bork the build
    The LIB variable from Visual Studio typically ends in a backslash.
    When building SDBM, the variable LIB is therefore set to something
    like "...\" which is an invalid string, of course.
    
    Since the LIB variable is utterly uninteresting, just set it to the
    empty string.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- perl/buildperl.sh
+++ perl/buildperl.sh
@@ -2,6 +2,6 @@
 PATH=/bin:$PATH
 ./Configure -dE -D cc=gcc -U libc= -U libs= -U libsfiles= -U libsfound= -U libswanted= -D runnm=false -D usenm=false -D so=dll -D usedl=false -D firstmakefile=GNUmakefile -D exe_ext=.exe -D _exe=.exe -D archname=msys
 ./Configure -S
-make
+LIB= make
 echo > ext.libs
-make
+LIB= make
