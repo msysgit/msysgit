e0c03e9506e180218084b3e55b6067f73ce10202
Sebastian Schuberth <sschuberth@visageimaging.com>
2007-10-05 10:01:28 +0200

    WinGit (Inno): Use Inno Setup from share so no installation is required
    Signed-off-by: Sebastian Schuberth <sschuberth@visageimaging.com>

--- share/WinGit-InnoSetup/release.sh
+++ share/WinGit-InnoSetup/release.sh
@@ -2,10 +2,6 @@
 
 test -z "$1" && {
 	echo "Usage: $0 <version>"
-	echo ""
-	echo "You need to have Inno Setup incl. Preprocessor from the QuickStart Pack"
-	echo "installed. Please download isetup-5.2.0.exe and ispack-5.2.0.exe from"
-	echo "<http://www.jrsoftware.org/isdl.php>."
 	exit 1
 }
 
@@ -17,14 +13,5 @@ TMPDIR=/tmp/WinGit
 cp gpl-2.0.txt install.bmp $TMPDIR &&
 homewinpath=$(cd ~ ; pwd -W) &&
 sed -e "s/%APPVERSION%/$version/" -e "s@%OUTPUTDIR%@$homewinpath@" install.iss >$TMPDIR/install.iss &&
-echo ""
-echo "Inno Setup will now be started for you. After it started you need to" &&
-echo "compile the loaded script (4th icon from the left in the toolbar)." &&
-echo "After compiling you will find the installer in \"$homewinpath\"." &&
-echo ""
-echo "NOTE: You should disconnect network drives before lauching Inno Setup." &&
-echo ""
-echo "Press RETURN to launch Inno Setup." &&
-read &&
-echo "Lauching Inno Setup ..." &&
-/bin/start "$TMPDIR/install.iss"
+echo "Lauching Inno Setup compiler ..." &&
+/share/InnoSetup/ISCC.exe "$TMPDIR/install.iss"
