82a580a7523884fa975007c21fb6a22f5968f445
Steffen Prohaska <prohaska@zib.de>
2007-10-15 16:56:01 +0200

    WinGit: refactored file copying to copy-files.sh
    Copying files in preparation for packing them into an installer
    is now available as the separate script 'copy-files.sh <tmpdir>'.
    The script is used by old WinGit and WinGit (Inno).
    
    Signed-off-by: Steffen Prohaska <prohaska@zib.de>

--- share/WinGit-InnoSetup/release.sh
+++ share/WinGit-InnoSetup/release.sh
@@ -8,8 +8,7 @@ test -z "$1" && {
 version=$1
 TMPDIR=/tmp/WinGit
 
-( echo "Calling WinGit installer to copy files ..." &&
-  cd /share/WinGit && ./release.sh $version ) &&
+/share/WinGit/copy-files.sh $TMPDIR &&
 cp gpl-2.0.txt install.bmp $TMPDIR &&
 homewinpath=$(cd ~ ; pwd -W) &&
 sed -e "s/%APPVERSION%/$version/" -e "s@%OUTPUTDIR%@$homewinpath@" install.iss >$TMPDIR/install.iss &&
--- share/WinGit/release.sh
+++ share/WinGit/release.sh
@@ -5,52 +5,14 @@ test -z "$1" && {
 	exit 1
 }
 
-test -d /doc/git/html/.git || { echo "Error: html pages in /doc/git/html/.git missing"; exit 1; }
-
 TARGET="$HOME"/WinGit-$1.exe
 OPTS7="-m0=lzma -mx=9 -md=64M"
 TARGET7=tmp.7z
 LIST7=etc/fileList.txt
 TMPDIR=/tmp/WinGit
 
-(test ! -d "$TMPDIR" || echo "Removing $TMPDIR" && rm -rf "$TMPDIR") &&
-mkdir "$TMPDIR" &&
-(cd "$(dirname "$0")" &&
- make &&
- mkdir "$TMPDIR"/bin &&
- cp broadcast-environment-change.exe "$TMPDIR"/bin &&
- cp create-shortcut.exe "$TMPDIR"/bin &&
- cp uninstaller.exe "$TMPDIR"/bin) &&
+"$(dirname $0)/copy-files.sh" $TMPDIR &&
 cd "$TMPDIR" &&
-echo "Copying files" &&
-(git --git-dir=/doc/git/html/.git log --pretty=format:%s -1 HEAD &&
- mkdir -p doc/git/html && cd doc/git/html &&
- git --git-dir=/doc/git/html/.git archive HEAD | tar xf -) &&
-(cd / && tar cf - bin/{git*,awk,basename.exe,bash.exe,bunzip2,bzip2.exe,\
-cat.exe,chmod.exe,clear,cmp.exe,cp.exe,cpio,cut.exe,cvs.exe,date.exe,diff.exe,\
-du.exe,echo,egrep,env.exe,expr.exe,false.exe,find.exe,gawk.exe,grep.exe,\
-gunzip,gzip.exe,head.exe,id.exe,less.exe,libW11.dll,libperl.dll,ln.exe,\
-ls.exe,md5sum.exe,mkdir.exe,msys-1.0.dll,msysltdl-3.dll,mv.exe,patch.exe,\
-patch.exe.manifest,perl.exe,printf,ps.exe,pwd,rm.exe,rmdir.exe,rxvt.exe,\
-scp.exe,sed.exe,sh.exe,sleep.exe,sort.exe,split.exe,ssh-agent.exe,ssh.exe,\
-tail.exe,tar.exe,tee.exe,touch.exe,tr.exe,true.exe,uname.exe,uniq.exe,vi,\
-vim.exe,wc.exe,which,xargs.exe,ssh-add.exe,start} cmd/ lib/perl5/ share/git* share/vim) |
-tar xvf - &&
-mkdir lib/perl5/site_perl &&
-cp /lib/{Error.pm,Git.pm} lib/perl5/site_perl/ &&
-gitmd5=$(md5sum bin/git.exe | cut -c 1-32) &&
-mkdir etc &&
-md5sum bin/git-*.exe | sed -n "s/^$gitmd5 \\*//p" > etc/fileList-builtins.txt &&
-rm $(cat etc/fileList-builtins.txt) &&
-(cd /mingw && tar cf - bin/*{tcl,tk,wish,gpg,curl.exe,libcurl}* \
-	lib/*{tcl,tk}* libexec/gnupg/) |
-tar xvf - &&
-strip bin/{[a-fh-z],g[a-oq-z]}*.exe &&
-cp /git/contrib/completion/git-completion.bash etc/ &&
-cp /etc/termcap etc/ &&
-sed 's/^\. .*\(git-completion.bash\)/. \/etc\/\1/' \
-	< /etc/profile > etc/profile &&
-cp /share/resources/git.ico etc/ &&
 sed "s/@@WINGITVERSION@@/$1/" < /share/WinGit/install.tcl > bin/install.tcl &&
 : > "$LIST7" &&
 find * -type f | sed "s|^\./||" > "$LIST7" &&
