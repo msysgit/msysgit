a16729f75e1811066fa19c07341fe1f550b43f88
Steffen Prohaska <prohaska@zib.de>
2007-10-16 22:49:33 +0200

    cmd: simplified command scripts
    As suggested by Dimitry Kakurin.
    
    Signed-off-by: Steffen Prohaska <prohaska@zib.de>

--- cmd/gitk.cmd
+++ cmd/gitk.cmd
@@ -1,8 +1,5 @@
 @echo off
-set MSYSGIT_SAVED_PATH=%PATH%
-set MSYSGIT_SAVED_HOME=%HOME%
+setlocal
 for /F "delims=" %%I in ("%~dp0..") do set path=%%~fI\bin;%%~fI\mingw\bin
 if "%HOME%"=="" set HOME=%USERPROFILE%
 start /B sh.exe /bin/gitk %*
-set PATH=%MSYSGIT_SAVED_PATH%
-set HOME=%MSYSGIT_SAVED_HOME%
