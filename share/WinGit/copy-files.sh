568e6aa35a3d1f6ffae30a275a3c716c8c81e1bc
Steffen Prohaska <prohaska@zib.de>
2007-10-27 22:07:39 +0200

    WinGit: include /etc/gitconfig in setup
    /etc/gitconfig sets core.symlink = false globally.
    We should include this into the setup.

--- share/WinGit/copy-files.sh
+++ share/WinGit/copy-files.sh
@@ -49,6 +49,7 @@ tar xvf - &&
 strip bin/{[a-fh-z],g[a-oq-z]}*.exe &&
 cp /git/contrib/completion/git-completion.bash etc/ &&
 cp /etc/termcap etc/ &&
+cp /etc/gitconfig etc/ &&
 cp /share/WinGit/ReleaseNotes.txt . &&
 sed 's/^\. .*\(git-completion.bash\)/. \/etc\/\1/' \
 	< /etc/profile > etc/profile &&
