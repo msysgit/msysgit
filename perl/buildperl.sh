da9ecdf9dd428eb50d04294ed63facdb11a5c503
Johannes Schindelin <johannes.schindelin@gmx.de>
2007-11-06 15:11:30 +0000

    buildperl: another try at automatically building it with dll support
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- perl/buildperl.sh
+++ perl/buildperl.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
-cd /perl/perl-5.6.1
 PATH=/bin:$PATH
-./Configure -dE -D cc=gcc -U libc= -U libs= -U libsfiles= -U libsfound= -U libswanted= -D runnm=false -D usenm=false -D so=dll -D usedl=false -D firstmakefile=GNUmakefile -D exe_ext=.exe -D _exe=.exe -D archname=msys
+
+cd /perl/perl-5.6.1
+test -f ext/DynaLoader/dl_win32.xs || cp win32/dl_win32.xs ext/DynaLoader/
+test -f ext/DynaLoader/win32.h || cp win32/win32.h ext/DynaLoader/
+
+./Configure -dE \
+	-D cc=gcc \
+	-U libc= \
+	-U libs= \
+	-U libsfiles= \
+	-U libsfound= \
+	-U libswanted= \
+	-D runnm=false \
+	-D usenm=false \
+	-D so=dll \
+	-D firstmakefile=GNUmakefile \
+	-D exe_ext=.exe -D _exe=.exe \
+	-D archname=msys \
+	-D dlsrc=dl_dlopen.xs -D dlext=dll -D usedl=define \
+	-D ccccdlflags=-fpic -D cccdlflags=-shared -D lddlflags=-shared \
+	-D optimize=-g \
+	2>&1 | tee config.log
 ./Configure -S
+
 LIB= make
