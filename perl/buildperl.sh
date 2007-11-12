8c346ec83fe2063aaaee85bc175391b9e1f3ef45
Simon Sasburg <Simon.Sasburg@gmail.com>
2007-11-12 20:42:51 +0100

    Allow perl to build with dll support.
    Signed-off-by: Simon Sasburg <Simon.Sasburg@gmail.com>

--- perl/buildperl.sh
+++ perl/buildperl.sh
@@ -1,27 +1,8 @@
 #!/bin/sh
-PATH=/bin:$PATH
+MSYSTEM=MSYS
 
 cd /perl/perl-5.6.1
-test -f ext/DynaLoader/dl_win32.xs || cp win32/dl_win32.xs ext/DynaLoader/
-test -f ext/DynaLoader/win32.h || cp win32/win32.h ext/DynaLoader/
 
-./Configure -dE \
-	-D cc=gcc \
-	-U libc= \
-	-U libs= \
-	-U libsfiles= \
-	-U libsfound= \
-	-U libswanted= \
-	-D runnm=false \
-	-D usenm=false \
-	-D so=dll \
-	-D firstmakefile=GNUmakefile \
-	-D exe_ext=.exe -D _exe=.exe \
-	-D archname=msys \
-	-D dlsrc=dl_dlopen.xs -D dlext=dll -D usedl=define \
-	-D ccccdlflags=-fpic -D cccdlflags=-shared -D lddlflags=-shared \
-	-D optimize=-g \
-	2>&1 | tee config.log
-./Configure -S
+./Configure -de
 
 LIB= make
