41d8b149c2c8af354cbba4b44e133b08dcffc6c1
Johannes Schindelin <johannes.schindelin@gmx.de>
2007-11-20 13:58:09 +0000

    Add /src/cvsps/
    Another automated build script.  This time for cvsps.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ src/cvsps/release.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cd "$(dirname "$0")" && . ../common/update-lib.sh
+
+
+package=cvsps
+version=yd
+url=git://repo.or.cz/cvsps-yd.git/
+d=cvsps-$version
+configure_options=--prefix=
+FILELIST=fileList.txt
+
+check_pristine && {
+	test -d "$d" || git clone $url
+} &&
+setup &&
+compile &&
+pre_install &&
+(cd $d && make install) &&
+post_install
+
