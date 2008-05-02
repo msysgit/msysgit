04190289ec710f23a6e703522d7b2aa80599f6c2
Steffen Prohaska <prohaska@zib.de>
2008-05-02 18:48:38 +0200

    Append PATH in git.cmd and gitk.cmd
    The old way of handling PATH in git.cmd and gitk.cmd was not perfect.
    git.cmd and gitk.cmd used to completely isolate the msysgit PATH from
    the system PATH.  One problem is that programs outside of the msysgit
    installation must be called with their full path, even if they are in
    the system PATH.
    
    This commit appends the system PATH after the msysgit paths, as
    suggested by Clifford Caoile.
    
    Signed-off-by: Steffen Prohaska <prohaska@zib.de>
    Tested-by: Clifford Caoile <piyo@users.sourceforge.net>

--- cmd/git.cmd
+++ cmd/git.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-for /F "delims=" %%I in ("%~dp0..") do set path=%%~fI\bin;%%~fI\mingw\bin
+for /F "delims=" %%I in ("%~dp0..") do set path=%%~fI\bin;%%~fI\mingw\bin;%PATH%
 if "%HOME%"=="" set HOME=%USERPROFILE%
 git.exe %*
 exit /b %ErrorLevel%
