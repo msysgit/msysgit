229a38207aea845b7bc37d27f13d1db7f45da82e
Marius Storm-Olsen <mstormo_git@storm-olsen.com>
2008-06-25 09:04:17 +0200

    Remove "@echo off", and prefix all other commands with '@' silencer.
    Since scripts which are called from these .cmd scripts may rely
    on ECHO being on, we don't touch the ECHO state, and rather silence
    every single command we do ourselves.
    
    Suggested-by: Stephan Hennig <mailing_list@arcor.de>
    Suggested-by: Roman Terekhov <roman.terekhov@gmail.com>
    Signed-off-by: Marius Storm-Olsen <mstormo_git@storm-olsen.com>

--- cmd/git.cmd
+++ cmd/git.cmd
@@ -1,6 +1,5 @@
-@echo off
-setlocal
-for /F "delims=" %%I in ("%~dp0..") do set path=%%~fI\bin;%%~fI\mingw\bin;%PATH%
-if "%HOME%"=="" set HOME=%USERPROFILE%
-git.exe %*
-exit /b %ErrorLevel%
+@setlocal
+@for /F "delims=" %%I in ("%~dp0..") do @set path=%%~fI\bin;%%~fI\mingw\bin;%PATH%
+@if "%HOME%"=="" @set HOME=%USERPROFILE%
+@git.exe %*
+@exit /b %ErrorLevel%
--- cmd/gitk.cmd
+++ cmd/gitk.cmd
@@ -1,5 +1,4 @@
-@echo off
-setlocal
-for /F "delims=" %%I in ("%~dp0..") do set path=%%~fI\bin;%%~fI\mingw\bin;%PATH%
-if "%HOME%"=="" set HOME=%USERPROFILE%
-start /B sh.exe /bin/gitk %*
+@setlocal
+@for /F "delims=" %%I in ("%~dp0..") do @set path=%%~fI\bin;%%~fI\mingw\bin;%PATH%
+@if "%HOME%"=="" @set HOME=%USERPROFILE%
+@start /B sh.exe /bin/gitk %*
