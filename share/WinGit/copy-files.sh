7f8c3dee623bc360d9ac0c0a2dec315375f489a2
Steffen Prohaska <prohaska@zib.de>
2008-09-22 08:14:44 +0200

    WinGit: Include missing libs for ssh
    The libraries were removed in c0ae32bbb5d702e5f5f054e2264ee13ade3171a3
    while removing svn.  But they are also needed by ssh.

--- share/WinGit/copy-files.sh
+++ share/WinGit/copy-files.sh
@@ -37,6 +37,7 @@ ls.exe,md5sum.exe,mkdir.exe,msys-1.0.dll,msysltdl-3.dll,mv.exe,patch.exe,\
 patch.exe.manifest,perl.exe,printf,ps.exe,pwd,rm.exe,rmdir.exe,rxvt.exe,\
 scp.exe,sed.exe,sh.exe,sleep.exe,sort.exe,split.exe,\
 ssh-agent.exe,ssh.exe,ssh-add.exe,ssh-keygen.exe,ssh-keyscan.exe,\
+msys-crypto-0.9.8.dll,msys-minires.dll,msys-z.dll,\
 tail.exe,tar.exe,tee.exe,touch.exe,tr.exe,true.exe,uname.exe,uniq.exe,vi,\
 vim.exe,wc.exe,which,xargs.exe,start} \
 cmd/ lib/perl5/ share/git* \
