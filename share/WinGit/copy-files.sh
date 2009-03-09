f58a107607c34ff46b1697e382e5a02c0eb9e7fe
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-03-09 11:13:47 +0100

    WinGit installer: include more vim files
    Noticed by Janos Laube, file list suggested by Christian Michon.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- share/WinGit/copy-files.sh
+++ share/WinGit/copy-files.sh
@@ -43,8 +43,12 @@ msys-crypto-0.9.8.dll,msys-ssl-0.9.8.dll,msys-minires.dll,msys-z.dll,\
 openssl.exe,vim,wc.exe,which,xargs.exe,start} lib/engines/ \
 ssl/ cmd/ lib/perl5/ share/git* \
 share/vim/vimrc share/vim/vim72/{filetype.vim,ftoff.vim,menu.vim,optwin.vim,\
-scripts.vim,syntax/c.vim,syntax/conf.vim,syntax/cpp.vim,syntax/gitcommit.vim,\
-syntax/synload.vim,syntax/syntax.vim,vim.exe}) |
+scripts.vim,\
+autoload/netrw.vim,autoload/netrwFileHandlers.vim,autoload/netrwSettings.vim,\
+plugin/netrwPlugin.vim,\
+syntax/c.vim,syntax/conf.vim,syntax/cpp.vim,syntax/diff.vim,\
+syntax/gitcommit.vim,syntax/syncolor.vim,syntax/synload.vim,syntax/syntax.vim,\
+vim.exe}) |
 tar xf - &&
 rm -rf lib/perl5/5.8.8/Encode/ lib/perl5/5.8.8/msys/auto/Encode/ bin/cvs.exe &&
 (test ! -f /lib/Git.pm || cp -u /lib/Git.pm lib/perl5/site_perl/Git.pm) &&
