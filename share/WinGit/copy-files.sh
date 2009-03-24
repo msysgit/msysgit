818960115c3d755c5424d5415a5c06d6f9e01009
Peter Harris <pharris@opentext.com>
2009-03-24 13:20:34 -0400

    Add pthreadGC2.dll to the list of shipped mingw files
    This allows the setting of THREADED_DELTA_SEARCH for faster repacks.
    
    Signed-off-by: Peter Harris <pharris@opentext.com>

--- share/WinGit/copy-files.sh
+++ share/WinGit/copy-files.sh
@@ -38,7 +38,7 @@ patch.exe.manifest,perl.exe,printf,ps.exe,pwd,rm.exe,rmdir.exe,rxvt.exe,\
 scp.exe,sed.exe,sh.exe,sleep.exe,sort.exe,split.exe,\
 ssh-agent.exe,ssh.exe,ssh-add.exe,ssh-keygen.exe,ssh-keyscan.exe,\
 tail.exe,tar.exe,tee.exe,touch.exe,tr.exe,true.exe,uname.exe,uniq.exe,vi,\
-msys-perl5_8.dll,lib{apr,aprutil,expat,neon,z,svn}*.dll,\
+msys-perl5_8.dll,lib{apr,aprutil,expat,neon,z,svn}*.dll,pthreadGC2.dll,\
 msys-crypto-0.9.8.dll,msys-ssl-0.9.8.dll,msys-minires.dll,msys-z.dll,\
 openssl.exe,vim,wc.exe,which,xargs.exe,start} lib/engines/ \
 ssl/ cmd/ lib/perl5/ share/git* \
