5504ec0c9cfab13eac2d99e3dc5a27650dd2005e
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-05-13 23:40:28 +0200

    Install Perl 5.8.8
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ mingw/bin/perlglob.bat
@@ -0,0 +1,69 @@
+@rem = '--*-Perl-*--
+@echo off
+if "%OS%" == "Windows_NT" goto WinNT
+perl -x -S "%0" %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto endofperl
+:WinNT
+perl -x -S %0 %*
+if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" goto endofperl
+if %errorlevel% == 9009 echo You do not have Perl in your PATH.
+if errorlevel 1 goto script_failed_so_exit_with_non_zero_val 2>nul
+goto endofperl
+@rem ';
+#!perl -w
+#line 15
+use File::DosGlob;
+$| = 1;
+while (@ARGV) {
+    my $arg = shift;
+    my @m = File::DosGlob::doglob(1,$arg);
+    print (@m ? join("\0", sort @m) : $arg);
+    print "\0" if @ARGV;
+}
+__END__
+
+=head1 NAME
+
+perlglob.bat - a more capable perlglob.exe replacement
+
+=head1 SYNOPSIS
+
+    @perlfiles = glob  "..\\pe?l/*.p?";
+    print <..\\pe?l/*.p?>;
+
+    # more efficient version
+    > perl -MFile::DosGlob=glob -e "print <../pe?l/*.p?>"
+
+=head1 DESCRIPTION
+
+This file is a portable replacement for perlglob.exe.  It
+is largely compatible with perlglob.exe (the Microsoft setargv.obj
+version) in all but one respect--it understands wildcards in
+directory components.
+
+It prints null-separated filenames to standard output.
+
+For details of the globbing features implemented, see
+L<File::DosGlob>.
+
+While one may replace perlglob.exe with this, usage by overriding
+CORE::glob with File::DosGlob::glob should be much more efficient,
+because it avoids launching a separate process, and is therefore
+strongly recommended.  See L<perlsub> for details of overriding
+builtins.
+
+=head1 AUTHOR
+
+Gurusamy Sarathy <gsar@activestate.com>
+
+=head1 SEE ALSO
+
+perl
+
+File::DosGlob
+
+=cut
+
+
+__END__
+:endofperl
---
+++ src/perl/release.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"
+
+url=ftp://ftp.funet.fi/pub/CPAN/src/
+version=5.8.8
+md5='b8c118d4360846829beb30b02a6b91a7'
+
+#dmakeurl=http://mirrors.zerg.biz/cpan/authors/id/S/SH/SHAY/
+#dmaketar=dmake-4.11-20080107-SHAY.zip
+dmakeurl=http://tools.openoffice.org/dmake/
+dmaketar=dmake_4.11.zip
+dmakedir=dmake
+
+if test ! -f $dmaketar
+then
+	curl $dmakeurl$dmaketar > $dmaketar
+fi &&
+if test ! -d $dmakedir
+then
+	unzip $dmaketar
+fi &&
+if test ! -f $dmakedir/Makefile
+then
+	perl -i.bak -pe 's/^_os=.*$/_os=MINGW/' $dmakedir/configure &&
+	(cd $dmakedir && ./configure)
+fi &&
+if test ! -f $dmakedir/dmake.exe
+then
+	(cd $dmakedir && make)
+fi || {
+	echo "Could not build dmake"
+	exit 1
+}
+
+p=perl-$version
+tar=$p.tar.gz
+test -f $tar || {
+	echo "Downloading $tar ..." 
+	curl $url/$tar -o $tar || exit
+}
+
+echo "Verifying md5 sum ..." 
+echo "$md5 *$tar" > $tar.md5
+md5sum -c --status $tar.md5 || exit
+
+test -d $p || {
+	echo "Unpacking $tar ..."
+	tar -xzf $tar || exit
+}
+
+cd $p || {
+	echo "Could not cd to $p"
+	exit 1
+}
+
+test -d .git || {
+	git init &&
+	git add . &&
+	git commit -m "Perl $version from $tar" > /dev/null ||
+	exit
+}
+
+#git rev-parse --verify HEAD^ 2>/dev/null ||
+#	git am ../patches/* ||
+#	exit
+
+test -f perl.exe || {
+	(cd win32 &&
+	TMP="$(echo "$TMP" | tr '/' '\\')" ../../$dmakedir/dmake.exe) ||
+	exit
+}
+
+index=$(/share/msysGit/pre-install.sh) &&
+(cd win32 &&
+TMP="$(echo "$TMP" | tr '/' '\\')" ../../$dmakedir/dmake install \
+INST_TOP="$(cd /mingw; pwd -W | tr '/' '\\')") &&
+/share/msysGit/post-install.sh $index "Install Perl $version" ||
+exit
+
+echo "Successfully built and installed Perl $version"
+echo "After checking the result, please commit (possibly with --amend)"
+echo
