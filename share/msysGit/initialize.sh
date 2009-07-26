cd0219ca421862b87579d364f938f393c77a5461
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-07-26 15:48:25 +0200

    Fix /share/msysGit/initialize.sh
    When msysGit was installed using the full installer, and the developer
    wants to turn this into a proper msysGit installation (read: with
    proper linking to the Git repositories), said script should help.
    
    Only that it did not work.
    
    Reported by Shin guey Wong (msysGit issue 291).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- share/msysGit/initialize.sh
+++ share/msysGit/initialize.sh
@@ -5,6 +5,7 @@
 test -d /git/.git || {
 	cd /git &&
 	git init &&
+	git config core.autocrlf false &&
 	git add . &&
 	git commit -m "Current revision" &&
 	git remote add -f origin mob@repo.or.cz:/srv/git/git/mingw/4msysgit.git
@@ -15,10 +16,12 @@ exit
 test -d /.git || {
 	cd / &&
 	git init &&
+	git config core.autocrlf false &&
 	git ls-files --other --exclude-standard $(sed -n \
 			-e 's/^path = /--exclude=/p' < .gitmodules) -z |
-		git update-index --add --stdin -z &&
+		git update-index --add -z --stdin &&
 	git commit -m "Current revision" &&
-	git remote add -f origin mob@repo.or.cz:/srv/git/4msysgit.git &&
+	git remote add -f origin \
+		mob@repo.or.cz:/srv/git/git/mingw/4msysgit.git &&
 	git gc
 }
