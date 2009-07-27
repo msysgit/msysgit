26d67bbc0be12da180eede78f80eec818ee17e95
Erik Faye-Lund <kusmabite@googlemail.com>
2009-07-27 21:10:20 +0200

    add a script to install libgsasl from source
    Signed-off-by: Erik Faye-Lund <kusmabite@gmail.com>

---
+++ src/libgsasl/release.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"
+
+VERSION=1.2
+DIR=libgsasl-$VERSION
+URL=http://mirrors.kernel.org/gnu/gsasl/$DIR.tar.gz
+FILE=${URL##*/}
+
+die () {
+	echo "$*" >&2
+	exit 1
+}
+
+test -d $DIR || {
+	test -f $FILE ||
+	curl -O $URL ||
+	die "Could not download $FILE"
+
+	tar xzvf $FILE && (
+		cd $DIR &&
+		git init &&
+		git add . &&
+		git commit -m "Import of $FILE"
+	)
+} || die "Could not check out $FILE"
+
+(cd $DIR &&
+./configure --prefix=/mingw &&
+make &&
+index=$(/share/msysGit/pre-install.sh) &&
+make install &&
+/share/msysGit/post-install.sh $index "Install $FILE"
+) || die "Could not install $FILE"
