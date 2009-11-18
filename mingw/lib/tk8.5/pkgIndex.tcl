ef1dc94cf18efeb2b1c0e4fd46a2ac2e7d47cd69
Pat Thoyts <patthoyts@users.sourceforge.net>
2009-11-18 15:45:57 +0000

    Update tk to version 8.5.8
    Signed-off-by: Pat Thoyts <patthoyts@users.sourceforge.net>

--- mingw/lib/tk8.5/pkgIndex.tcl
+++ mingw/lib/tk8.5/pkgIndex.tcl
@@ -1,2 +1,2 @@
-if {[catch {package present Tcl 8.5.0-8.6}]} { return }
-package ifneeded Tk 8.5.7	[list load [file join $dir .. .. bin tk85.dll] Tk]
+if {[catch {package present Tcl 8.5.0}]} { return }
+package ifneeded Tk 8.5.8	[list load [file join $dir .. .. bin tk85.dll] Tk]
--- mingw/lib/tk8.5/ttk/xpTheme.tcl
+++ mingw/lib/tk8.5/ttk/xpTheme.tcl
@@ -1,5 +1,5 @@
 #
-# $Id: xpTheme.tcl,v 1.6.2.1 2008/04/03 11:48:00 patthoyts Exp $
+# $Id: xpTheme.tcl,v 1.6.2.2 2009/05/14 00:53:04 patthoyts Exp $
 #
 # Settings for 'xpnative' theme
 #
@@ -31,15 +31,8 @@ namespace eval ttk::theme::xpnative {
 
 	# Treeview:
 	ttk::style configure Heading -font TkHeadingFont
-	ttk::style configure Row -background SystemWindow
-	ttk::style configure Cell -background SystemWindow
-	ttk::style map Row \
-	    -background [list selected SystemHighlight] \
-	    -foreground [list selected SystemHighlightText] ;
-	ttk::style map Cell \
-	    -background [list selected SystemHighlight] \
-	    -foreground [list selected SystemHighlightText] ;
-	ttk::style map Item \
+	ttk::style configure Treeview -background SystemWindow
+	ttk::style map Treeview \
 	    -background [list selected SystemHighlight] \
 	    -foreground [list selected SystemHighlightText] ;
 
