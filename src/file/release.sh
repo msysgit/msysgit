89fc9b6ba8fdb8c30300a9e2a545c46a1abe3427
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-12-07 16:30:35 +0100

    Add a script to install the file(1) tool
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ src/file/release.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"
+
+URL=ftp://ftp.astron.com/pub/file
+TAR=file-5.03.tar.gz
+DIR=file-5.03
+
+if test ! -f $TAR
+then
+	curl $URL/$TAR > $TAR
+fi &&
+if test ! -d $DIR
+then
+	tar xzvf $TAR
+fi &&
+(cd $DIR &&
+	git init &&
+	git add . &&
+	git commit -m initial &&
+	git am ../patches/* &&
+	CFLAGS=-I/git/compat/regex ./configure --prefix=/mingw &&
+	make &&
+	index=$(/share/msysGit/pre-install.sh) &&
+	make install &&
+	/share/msysGit/post-install.sh $INDEX Install $DIR
+)
