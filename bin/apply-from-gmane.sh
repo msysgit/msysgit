9c00caff1dab3c723e8d88c7cea9707290cc782d
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-01-14 10:28:24 +0100

    Add a script to download and apply patches directly from GMane
    This script was used to download (whitespace-fix) and apply the recent
    patch to add the NO_FSTAB_THREAD handling.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ bin/apply-from-gmane.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+URL="$1"
+
+case "$URL" in
+http://*|article.gmane.org*) ;;
+*) URL=http://article.gmane.org/gmane.comp.version-control.msysgit/$1;;
+esac
+
+case "$URL" in
+*/raw) ;;
+*) URL=$URL/raw;;
+esac
+
+curl $URL | git am --whitespace=fix -s
