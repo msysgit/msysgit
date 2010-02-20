a0e688d3e3bef42ee5bc66996c66eefd3e0a59b7
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-02-20 16:45:22 +0100

    Add a script to download & install MinGW-w64 gcc suite
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ src/mingw-w64/release-easy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"
+srcdir=$(pwd)
+
+# http://kent.dl.sourceforge.net/sourceforge/
+mirror=http://ignum.dl.sourceforge.net/project/mingw-w64/
+subdir=Toolchains%20targetting%20Win64/Automated%20Builds/
+file=mingw-w64-1.0-bin_i686-mingw_20100129.zip
+dir=${file%.zip}
+
+# download it
+test -f $file || curl $mirror$subdir$file > $file || exit
+
+# unpack & install it
+test -f sysroot/.root.init.marker &&
+test sysroot/.root.init.marker -nt $file ||
+(mkdir -p sysroot &&
+ cd sysroot &&
+ unzip ../$file &&
+ touch .root.init.marker
+)
