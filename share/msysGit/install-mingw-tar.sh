bebeedb527c44515b14f7de6022591ba4a61b495
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-02-20 14:05:52 +0100

    install-mingw-tar.sh: handle .lzma files
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- share/msysGit/install-mingw-tar.sh
+++ share/msysGit/install-mingw-tar.sh
@@ -13,8 +13,16 @@ index=$(/share/msysGit/pre-install.sh) || die "Pre-install stage failed"
 
 uncompress=
 case "$1" in
-*.bz2) uncompress=j;;
-*.gz|*.tgz) uncompress=z;;
+*.lzma)
+	(cd /tmp && /share/7-Zip/7za x "$1")
+	set /tmp/"$(basename "$1" .lzma)"
+	;;
+*.bz2)
+	uncompress=j
+	;;
+*.gz|*.tgz)
+	uncompress=z
+	;;
 esac
 
 tar x${uncompress}mf "$1" || die "Could not untar $1"
