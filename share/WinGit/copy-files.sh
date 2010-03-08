17b17b806e1ef76e98eb5c995a42aac2c98ae55b
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-03-08 14:39:29 +0100

    Include dirname in the WinGit installer
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- share/WinGit/copy-files.sh
+++ share/WinGit/copy-files.sh
@@ -38,6 +38,7 @@ echo "Copying files" &&
 $(ls {bin,libexec/git-core}/git* | grep -v 'cvs\|shell\|archimport\|instaweb') \
 bin/{awk,basename.exe,bash.exe,bunzip2,bzip2.exe,c_rehash,\
 cat.exe,chmod.exe,clear,cmp.exe,cp.exe,cut.exe,cvs.exe,date.exe,diff.exe,\
+dirname.exe,\
 du.exe,echo,egrep,env.exe,expr.exe,false.exe,find.exe,gawk.exe,grep.exe,\
 gunzip,gzip.exe,head.exe,id.exe,kill.exe,less.exe,libW11.dll,ln.exe,\
 ls.exe,md5sum.exe,mkdir.exe,msys-1.0.dll,msysltdl-3.dll,mv.exe,patch.exe,\
