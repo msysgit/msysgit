415460964ebc56d2dcaa2c65ea2b08ffaf629eec
Sebastian Schuberth <sschuberth@gmail.com>
2010-07-14 17:41:11 +0200

    Equalize git-cmd.bat to cmd/git.cmd
    This includes reordering some lines and the better fallback-logic for HOME.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- git-cmd.bat
+++ git-cmd.bat
@@ -1,7 +1,15 @@
-@set PLINK_PROTOCOL=ssh
+@rem Do not use "echo off" to not affect any child calls.
 @setlocal
+
+@rem Get the abolute path to the current directory, which is assumed to be the
+@rem Git installation root.
 @for /F "delims=" %%I in ("%~dp0") do @set git_install_root=%%~fI
-@set path=%git_install_root%\bin;%git_install_root%\mingw\bin;%git_install_root%\cmd;%PATH%
-@if "%HOME%"=="" @set HOME=%USERPROFILE%
+@set PATH=%git_install_root%\bin;%git_install_root%\mingw\bin;%git_install_root%\cmd;%PATH%
+
+@if not exist "%HOME%" @set HOME="%HOMEDRIVE%%HOMEPATH%"
+@if not exist "%HOME%" @set HOME="%USERPROFILE%"
+
+@set PLINK_PROTOCOL=ssh
+
 @cd %HOME%
 @start %COMSPEC%
