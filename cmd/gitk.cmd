def609eb9918217a9c73a71c50b4bae481199457
Sebastian Schuberth <sschuberth@gmail.com>
2010-08-05 08:08:48 +0200

    cmd: Remove the quotes introduced in 53e5b4faa; we did not have them before
    This should fix issue 512.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- cmd/gitk.cmd
+++ cmd/gitk.cmd
@@ -6,7 +6,7 @@
 @for /F "delims=" %%I in ("%~dp0..") do @set git_install_root=%%~fI
 @set PATH=%git_install_root%\bin;%git_install_root%\mingw\bin;%PATH%
 
-@if not exist "%HOME%" @set HOME="%HOMEDRIVE%%HOMEPATH%"
-@if not exist "%HOME%" @set HOME="%USERPROFILE%"
+@if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
+@if not exist "%HOME%" @set HOME=%USERPROFILE%
 
 @start "gitk" wish.exe "%git_install_root%\bin\gitk" -- %*
