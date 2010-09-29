1b423e4ed97fa5d39baf81812fe6de266eb20b9e
Jonathan Nieder <jrnieder@gmail.com>
2010-09-29 13:51:36 +0100

    file: update the file utility to version 5.04.
    This updates the release script to build version 5.04 and adds file
    checksum validation of the download.
    msysGit patches updated for the new release.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Pat Thoyts <patthoyts@users.sourceforge.net>

--- src/file/release.sh
+++ src/file/release.sh
@@ -3,13 +3,18 @@
 cd "$(dirname "$0")"
 
 URL=ftp://ftp.astron.com/pub/file
-TAR=file-5.03.tar.gz
-DIR=file-5.03
+TAR=file-5.04.tar.gz
+DIR=file-5.04
+MD5='accade81ff1cc774904b47c72c8aeea0'
 
 if test ! -f $TAR
 then
 	curl $URL/$TAR > $TAR
-fi &&
+fi
+
+echo "$MD5 *$TAR" > $TAR.md5
+md5sum -c --status $TAR.md5 || (echo "invalid checksum for $TAR"; exit 1)
+
 if test ! -d $DIR
 then
 	tar xzvf $TAR
