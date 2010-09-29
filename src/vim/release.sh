c46cbc9cf69a279d8acb3cf5059bbf49a638b1af
Jonathan Nieder <jrnieder@gmail.com>
2010-09-29 14:16:30 +0100

    vim: update release.sh to vim 7.3
    Vim 7.3 contains a number of bug fixes and a few new features,
    such as persistent undo and undo for reload.
    
    Upstream vim uses Mercurial rather than git.  Until remote-hg is
    usable, let's just use a tarball to grab the sources.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Pat Thoyts <patthoyts@users.sourceforge.net>

--- src/vim/release.sh
+++ src/vim/release.sh
@@ -2,18 +2,36 @@
 
 cd "$(dirname "$0")"
 
-URL=git://github.com/cmichon/vim.git
+VERSION=7.3
+DIR=vim73
+URL=ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
+MD5='5b9510a17074e2b37d8bb38ae09edbf2'
+FILE=${URL##*/}
 
-if test -d vim
-then
-	cd vim &&
-	git pull
-else
-	git clone $URL &&
-	cd vim
-fi &&
-cd src &&
-make -f Make_ming.mak vim.exe &&
+die () {
+	echo "$*" >&2
+	exit 1
+}
+
+test -d $DIR || {
+	test -f $FILE ||
+	curl -O $URL ||
+	die "Could not download vim"
+
+	echo "$MD5 *$FILE" > $FILE.md5
+	md5sum -c --status $FILE.md5 || (echo "invalid checksum $FILE";exit 1)
+
+	tar xjvf $FILE && (
+		cd $DIR &&
+		git init &&
+		git config core.autocrlf false &&
+		git add . &&
+		git commit -m "Import of $FILE"
+	)
+} || die "Could not check out vim"
+
+cd $DIR/src &&
+make -f Make_ming.mak GUI=no vim.exe &&
 INDEX=$(/share/msysGit/pre-install.sh) &&
 rm -rf /share/vim/vim[0-9]* &&
 VIM_VERSION=$(sed -n 's/.*VIM_VERSION_NODOT	"\(.*\)".*/\1/p' < version.h) &&
