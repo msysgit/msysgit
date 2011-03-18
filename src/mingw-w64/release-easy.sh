338c910a2f303f590b2da123d266296bb7c88743
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-03-18 12:18:29 +0100

    Update w64 fetch script to current mingw-w64
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src/mingw-w64/release-easy.sh
+++ src/mingw-w64/release-easy.sh
@@ -6,7 +6,7 @@ srcdir=$(pwd)
 # http://kent.dl.sourceforge.net/sourceforge/
 mirror=http://ignum.dl.sourceforge.net/project/mingw-w64/
 subdir=Toolchains%20targetting%20Win64/Automated%20Builds/
-file=mingw-w64-1.0-bin_i686-mingw_20100129.zip
+file=mingw-w64-bin_i686-mingw_20110314.zip
 dir=${file%.zip}
 
 # download it
