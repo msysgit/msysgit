msysgit/msysgit

commit eec84b05387d5408bd664a0a2d36873679019593

issue #535: support UNC paths for the "Git Bash Here" explorer menu item.
As pointed out in issue #535 the explorer integration menu item does not
open UNC paths. This patch creates a temporary shortcut item with the
target path set before it executes the bash program and solves this issue.

Suggested-by: Alex Ivanov <alexsi@aleksoft.net>
Signed-off-by: Pat Thoyts <patthoyts@users.sourceforge.net>

share/WinGit/Git Bash.vbs
@@ -1,10 +1,20 @@
-' If there is an argument, use it as the directory to change to.
-If WScript.Arguments.Length=1 Then
-    Set WshObj = CreateObject("WScript.Shell")
-    WshObj.CurrentDirectory = WScript.Arguments(0)
-End If
+Option Explicit
 
-' Launch the shortcut in the current directory which has the same
-' base name as this script.
-Set AppObj = CreateObject("Shell.Application")
-AppObj.ShellExecute(Replace(WScript.ScriptFullName, ".vbs", ".lnk"))
+Dim shell : Set shell = CreateObject("WScript.Shell")
+Dim fso : Set fso = CreateObject("Scripting.FileSystemObject")
+Dim gitdir : gitdir = Left(WScript.ScriptFullName,InStrRev(WScript.ScriptFullName,"\"))
+Dim bash : bash = fso.BuildPath(gitdir, "bin\sh.exe")
+Dim temp : temp = fso.GetSpecialFolder(2) ' Temporary folder
+Dim linkfile : linkfile = fso.BuildPath(temp, fso.GetTempName() & ".lnk")
+
+Dim link : Set link = shell.CreateShortCut(linkfile)
+link.TargetPath = bash
+link.Arguments = "--login -i"
+link.WindowStyle = 1
+If WScript.Arguments.Length > 0 Then link.WorkingDirectory = WScript.Arguments(0)
+link.Save
+
+Dim app : Set app = CreateObject("Shell.Application")
+app.ShellExecute linkfile
+WScript.Sleep 500
+fso.DeleteFile linkfile

commit bb9ea52d3da3655798b827dedfc03512e3943ae2

WinGit: Fix several regressions with the VB helper script
The changes in eec84b0 introduced several regressions:

- The window icon is no longer set to the Git icon
- The window title is a random name instead of "Git Bash"
- Changes to the window properties can no longer be saved because the
  dynamically created shortcut is automatically deleted

This commit fixes the above issues while maintaining compatibility with UNC
paths. This is done by:

- Specifying an icon location
- Always using "Git Bash" instead of a random name
- Not automatically deleting the shortcut from the temporary directory

The last two points should not be a problem as "CreateShortCut" does not
overwrite but opens existing shortcuts.

Finally, the code was cleaned-up and simplified.

Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>
Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

share/WinGit/Git Bash.vbs
@@ -1,20 +1,16 @@
-Option Explicit
+Set fso = CreateObject("Scripting.FileSystemObject")
+Set shell = CreateObject("WScript.Shell")
 
-Dim shell : Set shell = CreateObject("WScript.Shell")
-Dim fso : Set fso = CreateObject("Scripting.FileSystemObject")
-Dim gitdir : gitdir = Left(WScript.ScriptFullName,InStrRev(WScript.ScriptFullName,"\"))
-Dim bash : bash = fso.BuildPath(gitdir, "bin\sh.exe")
-Dim temp : temp = fso.GetSpecialFolder(2) ' Temporary folder
-Dim linkfile : linkfile = fso.BuildPath(temp, fso.GetTempName() & ".lnk")
+linkfile = fso.BuildPath(fso.GetSpecialFolder(TemporaryFolder), "Git Bash.lnk")
+gitdir = fso.GetParentFolderName(WScript.ScriptFullName)
 
-Dim link : Set link = shell.CreateShortCut(linkfile)
-link.TargetPath = bash
+' Dynamically create a shortcut with the current directory as the working directory.
+Set link = shell.CreateShortcut(linkfile)
+link.TargetPath = fso.BuildPath(gitdir, "bin\sh.exe")
 link.Arguments = "--login -i"
-link.WindowStyle = 1
+link.IconLocation = fso.BuildPath(gitdir, "etc\git.ico")
 If WScript.Arguments.Length > 0 Then link.WorkingDirectory = WScript.Arguments(0)
 link.Save
 
-Dim app : Set app = CreateObject("Shell.Application")
+Set app = CreateObject("Shell.Application")
 app.ShellExecute linkfile
-WScript.Sleep 500
-fso.DeleteFile linkfile