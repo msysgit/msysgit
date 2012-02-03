8bffd2c41408aef42c1ee639137fdfba84f93353
Heiko Voigt <hvoigt@hvoigt.net>
2012-02-04 11:08:32 +1300

    mingw-w64: Allow offline installation of 64-Bit toolchain
    If the user does not have a direct internet connection lets tell him
    what to download and where.
    
    Signed-off-by: Heiko Voigt <hvoigt@hvoigt.net>

--- src/mingw-w64/release-easy.sh
+++ src/mingw-w64/release-easy.sh
@@ -6,21 +6,37 @@ srcdir=$(pwd)
 # Limit the number of RSS feed entries.
 limit=500
 
-link=$(curl -s http://sourceforge.net/api/file/index/project-id/202880/mtime/desc/limit/$limit/rss |
-     sed -nr "s/<link>(.+(mingw-w64-bin_i686-mingw_[0-9]+.zip).+)<\/link>/\2\t\1/p" |
-     head -1)
+rss="$(curl -s http://sourceforge.net/api/file/index/project-id/202880/mtime/desc/limit/$limit/rss)"
+if test $? -ne 0
+then
+	file="$(ls -1 mingw-w64-bin_i686-mingw_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].zip 2>/dev/null)"
+	if test $? -ne 0
+	then
+		site=http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Automated%20Builds/
 
-file=$(echo "$link" | cut -f 1)
-url=$(echo "$link" | cut -f 2)
+		echo "Could not download mingw-w64-bin_i686-mingw_\$date.zip"
+		echo "If you do not have a direct network connection please go to"
+		echo "	$site"
+		echo "download the latest file, place it in $srcdir"
+		echo "and restart this script"
+		exit 1
+	else
+		file=$(echo "$file" | tail -n 1)
+	fi
+else
+	link=$(echo "$rss" |
+	     sed -nr "s/<link>(.+(mingw-w64-bin_i686-mingw_[0-9]+.zip).+)<\/link>/\2\t\1/p" |
+	     head -1)
+	file=$(echo "$link" | cut -f 1)
+	url=$(echo "$link" | cut -f 2)
 
-# Trim whitespaces.
-file=$(echo $file)
-url=$(echo $url)
+	# Trim whitespaces.
+	file=$(echo $file)
+	url=$(echo $url)
 
-dir=${file%.zip}
-
-# download it
-test -f $file || curl -L $url -o $file || exit
+	# download it
+	test -f $file || curl -L $url -o $file || exit
+fi
 
 # unpack & install it
 test -f sysroot/.root.init.marker &&
--- src/mingw-w64/release-zlib.sh
+++ src/mingw-w64/release-zlib.sh
@@ -8,7 +8,14 @@ file=zlib-1.2.6.tar.gz
 dir=${file%.tar.gz}
 
 # download it
-test -f $file || curl $mirror$file -o $file || exit
+if ! (test -f $file || curl $mirror$file -o $file)
+then
+	echo "Could not download $file"
+	echo "If you do not have a direct network connection please download"
+	echo "$mirror$file"
+	echo "place it in $srcdir and restart this script"
+	exit 1
+fi
 
 # unpack it
 test -d $dir || tar xzf $file || exit
