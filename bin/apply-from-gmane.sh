d29d16b27ae435bf603bd67182925e21cb425512
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-04-10 16:14:28 -0500

    apply-from-gmane: grok mid.gmane.org URLs and strip newlines from URLs
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/apply-from-gmane.sh
+++ bin/apply-from-gmane.sh
@@ -3,12 +3,16 @@
 URL="$1"
 
 case "$URL" in
+http://mid.gmane.org*)
+	TMP=.git/apply.tmp
+	curl -D $TMP "$URL" > /dev/null
+	URL=$(sed -n 's/^Location: //p' < $TMP | tr -d '\r\n');;
 http://thread.*) URL=http://article.${URL#http://thread.};;
 http://*|article.gmane.org*) ;;
 *@*)
 	TMP=.git/apply.tmp
 	curl -D $TMP http://mid.gmane.org/$1 > /dev/null
-	URL=$(sed -n 's/^Location: //p' < $TMP);;
+	URL=$(sed -n 's/^Location: //p' < $TMP | tr -d '\r\n');;
 *) URL=http://article.gmane.org/gmane.comp.version-control.msysgit/$1;;
 esac
 
