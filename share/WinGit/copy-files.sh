f909ee50e0d07d0cd55f3ab252666393f8a8a15c
Ciaran Jessup <ciaranj@gmail.com>
2012-05-29 23:02:39 +0100

    Adds mongoose.exe to the copy-files.sh script
    With this bundled we will have a working instaweb git sub-command
    without the end user having to make local configuration changes.
    
    The mongoose.exe is only 52Kb so does not bloat the installer by too
    much either!
    
    Signed-off-by: Ciaran Jessup <ciaranj@gmail.com>

--- share/WinGit/copy-files.sh
+++ share/WinGit/copy-files.sh
@@ -44,9 +44,9 @@ cat.exe,chmod.exe,clear,cmp.exe,cp.exe,cut.exe,cvs.exe,date.exe,diff.exe,\
 dirname.exe,\
 du.exe,echo,egrep,env.exe,expr.exe,false.exe,find.exe,flex.exe,gawk.exe,grep.exe,\
 head.exe,id.exe,kill.exe,less.exe,libW11.dll,ln.exe,\
-ls.exe,m4.exe,md5sum.exe,mkdir.exe,msys-1.0.dll,msysltdl-3.dll,mv.exe,patch.exe,\
-patch.exe.manifest,perl.exe,printf,ps.exe,pwd,recodetree,rm.exe,rmdir.exe,rxvt.exe,\
-scp.exe,sed.exe,sh.exe,sleep.exe,sort.exe,split.exe,\
+ls.exe,m4.exe,md5sum.exe,mkdir.exe,mongoose.exe,msys-1.0.dll,msysltdl-3.dll,mv.exe,\
+patch.exe,patch.exe.manifest,perl.exe,printf,ps.exe,pwd,recodetree,rm.exe,rmdir.exe,\
+rxvt.exe,scp.exe,sed.exe,sh.exe,sleep.exe,sort.exe,split.exe,\
 ssh-agent.exe,ssh.exe,ssh-add.exe,ssh-keygen.exe,ssh-keyscan.exe,\
 tail.exe,tar.exe,tee.exe,touch.exe,tr.exe,true.exe,uname.exe,uniq.exe,\
 unzip.exe,vi,\
