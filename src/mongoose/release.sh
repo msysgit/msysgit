45bc8df676bbb05cf49f34b68b88e7a37bc49d6a
Ciaran Jessup <ciaranj@gmail.com>
2012-05-29 23:02:00 +0100

    Add a script to install mongoose (3.1) from source
    Mongoose is a lightweight web server capable of serving cgi scripts.
    To provide a working instaweb function 'out of the box' for msysgit
    we need to be sure there is a web server we can host the gitweb.cgi
    script on.
    
    mongoose has no dependencies, compiles successfully without patching
    and results in a small (52Kb) executable so would appear to be an ideal
    candidate for shipping to re-enable this missing instaweb functionality
    in the deliverables.
    
    Signed-off-by: Ciaran Jessup <ciaranj@gmail.com>

---
+++ src/mongoose/release.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+VERSION=3.1
+DIR=mongoose
+URL=http://mongoose.googlecode.com/files/$DIR-$VERSION.tgz
+FILE=${URL##*/}
+
+die () {
+	echo "$*" >&2
+	exit 1
+}
+
+test -d $DIR || {
+	test -f $FILE ||
+	curl -O $URL ||
+	die "Could not download $FILE"
+
+	tar xzvf $FILE
+} || die "Could not check out $FILE"
+
+(cd $DIR &&
+index=$(/share/msysGit/pre-install.sh) &&
+make mingw &&
+strip mongoose.exe &&
+cp mongoose.exe /bin &&
+/share/msysGit/post-install.sh $index "Install $FILE"
+) || die "Could not install $FILE"
