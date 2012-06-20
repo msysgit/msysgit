aaf4a7cc0cb7d9e001ba76722b1e39590dfdd2a6
Pat Thoyts <patthoyts@users.sourceforge.net>
2012-06-20 16:28:07 +0100

    Revert "Add an up-to-date test for msys-1.0.dll"
    This reverts commit f08322cab3f54fa1cae5bf28c3f2b1540d3187e0
    
    This check no longer works as since 60527eb binaries may be modified on
    installation to speed up loading which changes the MD5 checksum on the
    users system.
    
    Signed-off-by: Pat Thoyts <patthoyts@users.sourceforge.net>

--- share/WinGit/copy-files.sh
+++ share/WinGit/copy-files.sh
@@ -83,7 +83,6 @@ fi &&
 	lib/{tcl,tk,dde,reg}* libexec/gnupg/) |
 tar xf - &&
 cp $MSYSGITROOT/mingw/bin/hd2u.exe bin/dos2unix.exe &&
-md5sum $MSYSGITROOT/bin/msys-1.0.dll > etc/msys-1.0.dll.md5 &&
 strip bin/{[a-fh-z],g[a-oq-z]}*.exe libexec/git-core/*.exe &&
 cp $MSYSGITROOT/git/contrib/completion/git-completion.bash etc/ &&
 cp $MSYSGITROOT/etc/termcap etc/ &&
