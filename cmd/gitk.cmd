dc273363dbab6f6ad9c4c18142277226a4732ac6
Ben Bodenmiller <bbodenmiller@hotmail.com>
2014-04-14 14:43:08 -0700

    fix typo
    Corrects spelling of absolute

--- cmd/gitk.cmd
+++ cmd/gitk.cmd
@@ -1,7 +1,7 @@
 @rem Do not use "echo off" to not affect any child calls.
 @setlocal
 
-@rem Get the abolute path to the parent directory, which is assumed to be the
+@rem Get the absolute path to the parent directory, which is assumed to be the
 @rem Git installation root.
 @for /F "delims=" %%I in ("%~dp0..") do @set git_install_root=%%~fI
 @set PATH=%git_install_root%\bin;%git_install_root%\mingw\bin;%PATH%
