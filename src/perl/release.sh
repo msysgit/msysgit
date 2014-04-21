c571eff5a7a6eb71076d203253d342993e6b9729
Olivier Mengué <dolmen@cpan.org>
2014-04-21 17:35:53 +0200

    perl/release.sh: use common/update-lib.sh

--- src/perl/release.sh
+++ src/perl/release.sh
@@ -1,74 +1,42 @@
 #!/bin/sh
 
-# Make sure that the working directory is clean and does not have untracked
-# files; otherwise our semi-automatic finding the new files will not work!
+cd "$(dirname "$0")" ; . ../common/update-lib.sh
 
-(cd / &&
-	 git diff-files --quiet &&
-	 git diff-index --cached --quiet HEAD &&
-	 test -z "$(git ls-files --exclude-from=.gitignore --exclude-per-directory=.gitignore --others)") || {
-		echo "State not pristine enough for successful Perl update"
-		exit 1
-	}
+check_pristine
 
-cd "$(dirname "$0")"
-
-# Remove old Perl files stored in fileList.txt
-
-cat fileList.txt | (cd / && xargs git rm) || exit
-
+package=perl
 url=ftp://ftp.funet.fi/pub/CPAN/src/5.0
 version=5.8.8
 md5='b8c118d4360846829beb30b02a6b91a7'
 
-p=perl-$version
-tar=$p.tar.gz
-test -f $tar || {
-	echo "Downloading $tar ..." 
-	curl $url/$tar -o $tar || exit
-}
+d=perl-$version
+tar=$d.tar.gz
+
+download
 
 echo "Verifying md5 sum ..." 
 echo "$md5 *$tar" > $tar.md5
 md5sum -c --status $tar.md5 || exit
 
-test -d $p || {
-	echo "Unpacking $tar ..."
-	tar -xzf $tar || exit
-}
+extract
 
-cd $p || {
-	echo "Could not cd to $p"
-	exit 1
-}
-
-test -d .git || {
-	git init &&
-	git add . &&
-	git commit -m "Perl $version from $tar" > /dev/null ||
-	exit
-}
-
-git rev-parse --verify HEAD^ 2>/dev/null ||
-	git am ../patches/* ||
-	exit
+apply_patches
 
 MSYSTEM=MSYS
+export MSYSTEM
 
-test -f config.sh || ./Configure -de || exit
+( cd "$d" ; test -f config.sh || ./Configure -de ) || die "Configure failure"
+
+compile
+
+die "DEBUG: aborting before install"
+exit 0
+
+FILELIST=fileList.txt
+pre_install
 
 # TODO! why is msys-perl5_8.dll not installed properly?
-LIB= make install || exit
+(cd "$d"; LIB= make install ) || exit
 
-# update index
-(cd / && git add .) || exit
+post_install
 
-git diff --cached --diff-filter=AM --name-only |
-	sed -e "s/^/\//" > fileList.txt ||
-	exit
-		
-git add fileList.txt || exit
-		
-echo "Successfully built and installed Perl $version"
-echo "After checking the result, please commit (possibly with --amend)"
-echo
