7649771d912836c1c588c2923030ac06c4ced1c0
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-06-12 09:54:03 -0500

    download-stats: look for 1.9.4 now
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/download-stats.sh
+++ bin/download-stats.sh
@@ -3,6 +3,10 @@
 # This script shows the download stats on GitHub. Update the id using the
 # output of curl -s https://api.github.com/repos/msysgit/msysgit/releases/
 
-id=${1:-268568}
+# 1.9.2
+#id=${1:-268568}
+# 1.9.4
+id=${1:-371088}
+
 curl -s https://api.github.com/repos/msysgit/msysgit/releases/$id/assets |
 grep -e '"name":' -e '"download_count":'
