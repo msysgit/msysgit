0a4aa37b44525fcff95b2f521701538255f974c9
Danny B <silverkorn@gmail.com>
2014-07-07 21:44:14 -0400

    Fixed setting variables in current session. Inserted "endlocal" strangely in the loop but effectively to call the command first then set the variables outside of the batch file.

--- cmd/setup-ssh-agent.cmd
+++ cmd/setup-ssh-agent.cmd
@@ -13,6 +13,7 @@ echo Configuring SSH Agent environment...
 :: Start and set environment variables for SSH agent and key insertion
 @set GIT_ENV_UTILITY_DIR=%~dp0..\etc\
 for /f "delims=" %%a in ('cscript.exe /nologo ^"!GIT_ENV_UTILITY_DIR!git-env-utility.js^" /gitbindir:p /p /home:v /startsshagent:v') do (
+    endlocal
     @set %%a
 )
 
