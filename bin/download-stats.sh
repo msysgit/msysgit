e4d948b19a5959d5c692e66d24ca0a9adf911653
Thomas Braun <thomas.braun@byte-physics.de>
2014-08-15 19:18:58 +0200

    download-stat: look for 1.9.4.msysgit.1 now
    Signed-off-by: Thomas Braun <thomas.braun@byte-physics.de>

--- bin/download-stats.sh
+++ bin/download-stats.sh
@@ -6,7 +6,9 @@
 # 1.9.2
 #id=${1:-268568}
 # 1.9.4
-id=${1:-371088}
+#id=${1:-371088}
+# 1.9.4.msysgit.1
+id=${1:-494232}
 
 curl -s https://api.github.com/repos/msysgit/msysgit/releases/$id/assets |
 grep -e '"name":' -e '"download_count":'
