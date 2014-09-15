a6018d3fe8a4f618361f5d0a9c0e1ba0b0f3c1d8
ADTC <send2adtc@gmail.com>
2014-09-15 17:38:28 +0800

    Use delayed expansion of environment variables
    With the immediate expansion, paths containing the ampersand symbol (&)
    will not work in this script. This is likely to occur when the user
    profile is located in a folder that contains the symbol in its name.
    The command processor misinterprets the symbol as to mean the union of
    two separate commands.
    
    To avoid the issue, "delayed expansion" is used and the environment
    variables are demarcated using the exclamation mark (!) instead of
    the percentage symbol (%).
    
    Signed-off-by: ADTC <send2adtc@gmail.com>

--- cmd/gitk.cmd
+++ cmd/gitk.cmd
@@ -1,12 +1,12 @@
 @rem Do not use "echo off" to not affect any child calls.
-@setlocal
+@setlocal enableDelayedExpansion
 
 @rem Get the absolute path to the parent directory, which is assumed to be the
 @rem Git installation root.
 @for /F "delims=" %%I in ("%~dp0..") do @set git_install_root=%%~fI
-@set PATH=%git_install_root%\bin;%git_install_root%\mingw\bin;%PATH%
+@set PATH=!git_install_root!\bin;!git_install_root!\mingw\bin;!PATH!
 
-@if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
-@if not exist "%HOME%" @set HOME=%USERPROFILE%
+@if not exist "!HOME!" @set HOME=!HOMEDRIVE!!HOMEPATH!
+@if not exist "!HOME!" @set HOME=!USERPROFILE!
 
-@start "gitk" wish.exe "%git_install_root%\bin\gitk" -- %*
+@start "gitk" wish.exe "!git_install_root!\bin\gitk" -- %*
