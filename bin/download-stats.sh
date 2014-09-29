fc0c8218091f10ec26885c7fe63cc2811344994d
Thomas Braun <thomas.braun@byte-physics.de>
2014-09-30 00:56:57 +0200

    Update download stats for 1.9.4.msysgit.2
    Signed-off-by: Thomas Braun <thomas.braun@byte-physics.de>

--- bin/download-stats.sh
+++ bin/download-stats.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 # This script shows the download stats on GitHub. Update the id using the
-# output of curl -s https://api.github.com/repos/msysgit/msysgit/releases/
-
+# output of curl -s https://api.github.com/repos/msysgit/msysgit/releases
 # 1.9.2
 #id=${1:-268568}
 # 1.9.4
 #id=${1:-371088}
 # 1.9.4.msysgit.1
-id=${1:-494232}
+#id=${1:-494232}
+# 1.9.4.msysgit.2
+id=${1:-591437}
 
 curl -s https://api.github.com/repos/msysgit/msysgit/releases/$id/assets |
 grep -e '"name":' -e '"download_count":'
