372ce03958e141dd9d55b0e9ae2efce0496215c8
KernelJ <kernelj@epixxware.com>
2015-01-25 14:56:21 +0000

    New cleanup.sh for /src builds to remove unneeded files after install.

---
+++ src/cleanup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+rm -rf WhoUses/WhoUses.exe WhoUses/*.o
+rm -rf antiword/antiword-git
+rm -rf ccache-win32/ccache-svn
+rm -rf connect/connect.exe
+rm -rf curl/curl-7.30.0*
+rm -rf depends/depends.exe depends/*.o
+rm -rf expat/expat-2.1.0*
+rm -rf file/file-5.04*
+rm -rf getcp/getcp.exe getcp/getcp.o
+rm -rf gettext/gettext-0.18.1.1*
+make -C git-cheetah clean
+rm -rf git-wrapper/git-wrapper.exe git-wrapper/git-wrapper.o
+rm -rf gnupg/gnupg-1.4.13*
+rm -rf gzip/gzip-1.2.4*
+rm -rf hexdump/hexdump-1.7*
+rm -rf libgsasl/libgsasl-1.2*
+rm -rf libiconv/libiconv-1.14*
+rm -rf msmtp/msmtp-1.4.17*
+rm -rf openssl/openssl-0.9.8zd*
+rm -rf poppler/poppler-0.14.5*
+rm -rf rebase/rebase-2.4.2-1*
+rm -rf tcltk/tcl8.5.13*
+rm -rf tcltk/tk8.5.13*
+rm -rf tcltk/tkimg1.3*
+rm -rf unzip/unzip60*
+rm -rf vim/vim-7.4*
+rm -rf vim/vim74
+rm -rf xz/xz-5.2.0*
+rm -rf zlib/zlib-1.2.8*
