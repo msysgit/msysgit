a4c24dc6da673cea3687d8254be7fcc69f9f74da
KernelJ <kernelj@epixxware.com>
2015-01-25 14:49:48 +0000

    Updated release script for connect

--- src/connect/release.sh
+++ src/connect/release.sh
@@ -2,6 +2,7 @@
 
 cd "$(dirname "$0")" &&
 exe=/mingw/bin/connect.exe
-gcc -O2 -o $exe connect.c -lws2_32 &&
+gcc -O2 -o connect.exe connect.c -lws2_32 &&
+cp connect.exe $exe &&
 git add $exe &&
 git commit -n -s -m "Commit connect, a SOCKS proxy" $exe
